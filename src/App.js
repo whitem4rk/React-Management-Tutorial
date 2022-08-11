8d0cafb135f302fd7d5363f056142fe5d8aaf70c
whitemark <whtmdgus56@nate.com>
2022-08-11 15:14:45 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>develop</h2>
-    </div>
-  );
+const customers = [
+  {
+    'id':1,
+    'image': 'https://placeimg.com/64/64/any',
+    'name':'james',
+    'birthday' : '12123',
+    'gender' : 'm',
+    'job':'studnet'
+  },
+  {
+    'id':2,
+    'image': 'https://placeimg.com/64/64/any',
+    'name':'james',
+    'birthday' : '12123',
+    'gender' : 'm',
+    'job':'studnet'
+  },
+  {
+    'id':3,
+    'image': 'https://placeimg.com/64/64/any',
+    'name':'james',
+    'birthday' : '12123',
+    'gender' : 'm',
+    'job':'studnet'
+  }
+]
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+      </div> 
+    );
+  }
 }
 
 export default App;

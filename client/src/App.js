618d608228ced80f5d24fab3ddd4ba3093ad828a
whitemark <whtmdgus56@nate.com>
2022-08-14 22:28:28 +0900

    delete

--- client/src/App.js
+++ client/src/App.js
@@ -64,11 +64,12 @@ progress = () => {
                 <TableCell>생년월일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {this.state.customers ? this.state.customers.map(c => { return (
-                <Customer
+                <Customer stateRefresh={this.stateRefresh}
                   key={c.id}
                   id={c.id}
                   image={c.image}
